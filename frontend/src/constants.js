574b1abfa1461ebdaa35683ce48712fbb980a63c
Insuline <insuline.ru@gmail.com>
2022-01-14 15:59:57 +0300

    refactor(frontend): добавил json файл c abi в репо фронтенда

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,4 +1,4 @@
-import { abi } from '../../contract/artifacts/contracts/SendCryptoMessages.sol/SendCryptoMessages.json'
+import { abi } from '@/utils/SendCryptoMessages.json'
 
 export const CONTRACT_ADDRESS = '0xc8Ab129c4b42fFd424C8b92227Cdde634d31f1ef'
 export const CONTRACT_ABI = abi
